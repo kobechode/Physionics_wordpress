4cb6b775c6568da221ead0be447d6371e73a2df1
kobechode <homycode@gmail.com>
2023-04-12 14:32:23 +0800

    asdasdsd

--- footer.php
+++ footer.php
@@ -28,20 +28,25 @@
                           array(
                             'taxonomy' => 'category',
                             'field' => 'slug',
-                            'terms' => 'company'
+                            'terms' => 'Company_footer'
                           )
                         )
                     );
                     $newQuery = new WP_Query($args);
                     ?>  
                         <?php if($newQuery->have_posts()) : while($newQuery->have_posts()) : $newQuery->the_post(); ?>
-                        <?php if(get_field('company')) :while(the_repeater_field('company')):?>
-                    <ul>
-                    <li> <h2><?php echo get_field('company_info');?></h2></li>
-                    <li> <h2><?php echo get_field('company_info');?></h2></li>
-                    <li> <h2><?php echo get_field('company_info');?></h2></li>
-                    <li> <h2><?php echo get_field('company_info');?></h2></li>
-                    <li> <h2><?php echo get_field('company_info');?></h2></li>
+                    
+
+                          <div>
+                          <?php if(get_field('company')) :while(the_repeater_field('company')):?>
+                          <ul>
+                    <li>  <?php echo get_sub_field('company_info');?> </li>
+                    <li>  <?php echo get_sub_field('about_us');?></h2></li>
+                    <li>  <?php echo get_sub_field('we_are_hiring');?></h2></li>
+                    <li>  <?php echo get_sub_field('blog');?></h2></li>
+                 
+
+                    </ul>
                     <?php 
 
 endwhile;
@@ -50,8 +55,10 @@ endwhile;
 endif;
     wp_reset_postdata();
 
-   ?>
-                    </ul>
+     ?>
+                          
+                          </div>
+                
                     <?php 
 
                 endwhile;
@@ -61,20 +68,99 @@ endif;
                     wp_reset_postdata();
 
                      ?>
-                    <ul>
-                        <li>Features</li>
-                        <li>Business Marketing</li>
-                        <li>User Analytic</li>
-                        <li>Live Chat</li>
-                        <li>Unlimited Support</li>
+
+                   <?php
+                   
+                    $args = array(
+                        'post_type' => 'Physionics',
+                        'posts_per_page' => 5,
+                        'tax_query' => array(
+                          array(
+                            'taxonomy' => 'category',
+                            'field' => 'slug',
+                            'terms' => 'Footer_Features'
+                          )
+                        )
+                    );
+                    $newQuery = new WP_Query($args);
+                    ?> 
+              <?php if($newQuery->have_posts()) : while($newQuery->have_posts()) : $newQuery->the_post(); ?>
+                     <div class="features_footer">
+                     <?php if(get_field('features')) :while(the_repeater_field('features')):?>
+                    
+                     <ul>
+                         <li>  <?php echo get_sub_field('features_1');?></li>
+                         <li>  <?php echo get_sub_field('business_marketing');?></li>
+                         <li>  <?php echo get_sub_field('user_analytic');?></li>
+                         <li>  <?php echo get_sub_field('live_chat');?></li>
+                         <li>  <?php echo get_sub_field('unlimited_support');?></li>
                     </ul>
-                    <ul>
-                        <li>Resources</li>
-                        <li>IOS & Android</li>
-                        <li>Watch a Demo</li>
-                        <li>Customers</li>
-                        <li>API</li>
+                    <?php 
+
+endwhile;
+    else : 
+        echo "no available post yet";
+endif;
+    wp_reset_postdata();
+
+    ?>
+                     </div>
+                     <?php 
+
+                  endwhile;
+                      else : 
+                          echo "no available post yet";
+                  endif;
+                      wp_reset_postdata();
+
+                      ?>
+
+<?php
+                   
+                   $args = array(
+                       'post_type' => 'Physionics',
+                       'posts_per_page' => 5,
+                       'tax_query' => array(
+                         array(
+                           'taxonomy' => 'category',
+                           'field' => 'slug',
+                           'terms' => 'Resources_Footer'
+                         )
+                       )
+                   );
+                   $newQuery = new WP_Query($args);
+                   ?> 
+  <?php if($newQuery->have_posts()) : while($newQuery->have_posts()) : $newQuery->the_post(); ?>
+               <div class="resources">
+               <?php if(get_field('footer_resources')) :while(the_repeater_field('footer_resources')):?>
+               <ul>
+               <li>  <?php echo get_sub_field('resources');?></li>
+                         <li>  <?php echo get_sub_field('system');?></li>
+                         <li>  <?php echo get_sub_field('demo');?></li>
+                         <li>  <?php echo get_sub_field('customers');?></li>
+                         <li>  <?php echo get_sub_field('api');?></li>
                     </ul>
+                    <?php 
+
+endwhile;
+    else : 
+        echo "no available post yet";
+endif;
+    wp_reset_postdata();
+
+    ?>
+
+               </div>
+               <?php 
+
+              endwhile;
+                  else : 
+                      echo "no available post yet";
+              endif;
+                  wp_reset_postdata();
+
+                  ?>
+                  
                 </div>
             </div>
         </footer>
